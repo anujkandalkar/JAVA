b3bb4de25f8c215d4ad609e140541172b25de0e5
anujkandalkar <anujkandalkar78@gmail.com>
2026-07-14 00:49:44 +0530

    Add ThreeSum solution

---
+++ Array/ThreeSum.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int s = nums[i] + nums[l] + nums[r];
+                if (s == 0) {
+                    answer.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                } else if (s < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(s.threeSum(nums));
+    }
+}
