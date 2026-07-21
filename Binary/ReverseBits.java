da0a0259c97c14b37b480b85aa2b9532b24edfeb
anujkandalkar <anujkandalkar78@gmail.com>
2026-07-21 22:44:58 +0530

    Add ReverseBits solution

---
+++ Binary/ReverseBits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1);
+            n >>>= 1;
+        }
+        return result;
+    }
+}
+
+public class ReverseBits {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int result = s.reverseBits(43261596);
+        System.out.println(result);
+    }
+}
